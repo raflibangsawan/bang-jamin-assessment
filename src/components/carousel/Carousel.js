3611de4147e8a8cd350246eaf221ee0d520de451
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-04 01:52:43 +0700

    changed the swipe feel smoother

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,20 +1,40 @@
-import React from "react";
-import { FlatList, View } from "react-native";
+import React, { useState, useRef } from "react";
+import { FlatList, View, Animated } from "react-native";
 
 import CarouselItem from "../carouselItem/CarouselItem";
 import slides from "./slides";
 
 const Carousel = () => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const slidesRef = useRef(null);
+  const viewableItemsChanged = useRef(({ viewableItems }) =>
+    setCurrentIndex(viewableItems[0].index)
+  ).current;
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <FlatList
-        data={slides}
-        renderItem={({ item }) => <CarouselItem item={item} />}
-        horizontal
-        showsHorizontalScrollIndicator
-        pagingEnabled
-        bounces={false}
-      />
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => <CarouselItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator
+          pagingEnabled
+          bounces={false}
+          keyExtractor={(item) => item.id}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: false }
+          )}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
     </View>
   );
 };
