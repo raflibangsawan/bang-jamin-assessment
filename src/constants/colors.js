1abaa2070ffe8071b3daade2d1fb9d006ff5e7c6
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-04 00:19:44 +0700

    added constants for color and fontsize

---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  primary: "#EA5F2A",
+  white: "#ffffff",
+  gray: "#878787",
+  black: "#0F0F0F",
+};
+
+export default colors;
---
+++ src/constants/fontSize.js
@@ -0,0 +1,9 @@
+const fontSize = {
+  reguler: "14px",
+  h1: "30px",
+  h2: "24px",
+  h3: "16px",
+  small: "12px",
+};
+
+export default fontSize;
