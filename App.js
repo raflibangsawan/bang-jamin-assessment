dfcb2c8853a15c765bf024dac7e5529b480f022f
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-04 00:19:15 +0700

    import fonts

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Welcome from "./src/pages/Welcome/Welcome";
 
 const Stack = createStackNavigator();
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+  const [isLoaded] = useFonts({
+    "mul-mid": require("./assets/fonts/Mulish-Medium.ttf"),
+    "chi-mid": require("./assets/fonts/Chillax-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    const hideSplash = async () => {
+      if (isLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    hideSplash();
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
