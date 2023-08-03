b812e839768bb22c5f58fa0de6440a6ecb74e26c
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-04 04:18:28 +0700

    added image in login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Image, View } from "react-native";
 import React from "react";
+import colors from "../../constants/colors";
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../../../assets/images/login-image.png")}
+        style={styles.image}
+      />
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    height: "100%",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+    maxHeight: "65%",
+  },
+});
