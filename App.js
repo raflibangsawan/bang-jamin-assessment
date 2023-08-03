f8c434663c5631b67a44d10ee271a38d1dec5044
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-03 18:30:44 +0700

    added welcome page component to the app

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import Welcome from "./src/pages/Welcome/Welcome";
+import { Welcome } from "./src/pages/Welcome/Welcome";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -21,12 +20,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
