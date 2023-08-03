4256768903f17d618b80e958d94aa3f8c0bb3e68
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-03 18:31:15 +0700

    added style to welcome page

---
+++ src/pages/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { SafeAreaView, View, ScrollView } from "react-navigation";
+import { Text } from "react-native";
+
+import { styles } from "./styles";
+
+const Welcome = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+};
+
+export default Welcome;
--- src/pages/Welcome/Welcome.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import { Text } from "react-native";
-
-const Welcome = () => {
-  return <Text>Welcome</Text>;
-};
-
-export default Welcome;
