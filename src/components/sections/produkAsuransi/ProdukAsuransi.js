530f92f7473df8d392983ac68bccf4647890dce6
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-05 03:34:22 +0700

    added section component

---
+++ src/components/sections/produkAsuransi/ProdukAsuransi.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+import styles from "./styles";
+
+const ProdukAsuransi = () => {
+  return (
+    <View>
+      <Text>ProdukAsuransi</Text>
+    </View>
+  );
+};
+
+export default ProdukAsuransi;
---
+++ src/components/sections/ruangKerja/RuangKerja.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import styles from "./styles";
+
+const RuangKerja = () => {
+  return (
+    <View>
+      <Text>RuangKerja</Text>
+    </View>
+  );
+};
+
+export default RuangKerja;
+
+const styles = StyleSheet.create({});
