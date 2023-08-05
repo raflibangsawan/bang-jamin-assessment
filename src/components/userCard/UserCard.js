c76512fb10bb21cc5dc71263f86b68ee69e88a63
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-05 12:57:44 +0700

    implemented userCard in nasabah page

--- src/components/userCard/UserCard.js
+++ src/components/userCard/UserCard.js
@@ -1,11 +1,27 @@
-import { Text, View } from "react-native";
+import { View, Image } from "react-native";
 import React from "react";
 import styles from "./styles";
 
-const UserCard = () => {
+import TextRegulerBold from "../fonts/TextRegulerBold";
+import TextRegulerSemiBold from "../fonts/TextRegulerSemiBold";
+
+const UserCard = ({ name, email, image }) => {
   return (
     <View style={styles.container}>
-      <Text>UserCard</Text>
+      <View style={styles.contentContainer}>
+        <Image
+          source={{ uri: "https://reqres.in/img/faces/1-image.jpg" }}
+          style={styles.infoImageIcon}
+        />
+        <View>
+          <TextRegulerSemiBold>Rafli Bangsawan</TextRegulerSemiBold>
+          <TextRegulerBold>raflibangsawans@ui.ac.id</TextRegulerBold>
+        </View>
+      </View>
+
+      <View style={styles.dividerContainer}>
+        <View style={styles.divider} />
+      </View>
     </View>
   );
 };
--- src/components/userCard/styles.js
+++ src/components/userCard/styles.js
@@ -1,5 +1,33 @@
 import { StyleSheet } from "react-native";
+import colors from "../../constants/colors";
 
-const styles = StyleSheet.create({ container: {} });
+const styles = StyleSheet.create({
+  container: { width: "100%" },
+  contentContainer: {
+    padding: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 18,
+  },
+  infoImageIcon: {
+    resizeMode: "contain",
+    width: 65,
+    height: 65,
+    borderRadius: 99,
+  },
+  dividerContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: 1,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: colors.gray,
+    opacity: 0.1,
+    marginVertical: 10,
+    width: "90%",
+  },
+});
 
 export default styles;
