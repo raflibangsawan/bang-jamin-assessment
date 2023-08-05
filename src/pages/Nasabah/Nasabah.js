34ac54f26c8725e942caaa2829a5a13e20876a02
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-05 10:19:04 +0700

    added Nasabah page

---
+++ src/pages/Nasabah/Nasabah.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+import styles from "./styles";
+
+const Nasabah = () => {
+  return (
+    <View>
+      <Text>Nasabah</Text>
+    </View>
+  );
+};
+
+export default Nasabah;
