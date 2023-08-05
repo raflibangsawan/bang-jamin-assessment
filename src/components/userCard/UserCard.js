40146d730446bba557aa4151dea411ca5b6f6d4d
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-08-05 11:05:07 +0700

    added userCard component

---
+++ src/components/userCard/UserCard.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+import styles from "./styles";
+
+const UserCard = () => {
+  return (
+    <View style={styles.container}>
+      <Text>UserCard</Text>
+    </View>
+  );
+};
+
+export default UserCard;
